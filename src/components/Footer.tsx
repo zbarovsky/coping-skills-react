560a63205ff4064079d93576db6455b979940d54
Zack Barovsky <57148341+zbarovsky@users.noreply.github.com>
2021-04-14 15:15:07 -0700

    updated footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,26 @@ import React from 'react'
 function Footer() {
 
     return (
-        <div>
-            <p>footer shit goes here</p>
+        <div className='footer-container'>
+            <div className='copyright'>
+               <p>© Zack Barovsky 2021</p> 
+            </div>
+            <div className='footer-social-media'>
+                <ul>
+                    <li>
+                        LinkedIn
+                    </li>
+                    <li>
+                        GitHub
+                    </li>
+                    <li>
+                        Twitter
+                    </li>
+                    <li>
+                        Instagram
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
