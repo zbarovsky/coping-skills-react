358d6456df3c311141fb82ddd64d590cc7fde349
Zack Barovsky <57148341+zbarovsky@users.noreply.github.com>
2021-04-15 11:28:58 -0700

    api connected, returns correct coping skill

--- src/components/CopingSkill.tsx
+++ src/components/CopingSkill.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import axios from 'axios'
+import React, {useState, useEffect} from 'react'
 
 function CopingSkill() {
 
+    const [copingSkill, setCopingSkill] = useState<object>()
+
+    useEffect(() => {
+        let copingSkillId: number = Math.floor(Math.random() * 100)
+        console.log(copingSkillId)
+        axios.get(`http://127.0.0.1:5000/api/copingskill/`, {params: {id: copingSkillId}})
+        .then(response => {
+            console.log(response, typeof response)
+            setCopingSkill(response.data.skill)
+        })
+    }, [])
+
     return (
-        <div>
-            <p>return coping skill goes here</p>
+        <div className="coping-skill">
+            {copingSkill}
         </div>
     )
 }
