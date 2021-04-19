aea1c1ea79c4f4191d06d11ff800095cf71d7e36
Zack Barovsky <57148341+zbarovsky@users.noreply.github.com>
2021-04-19 15:27:06 -0700

    push for fixing deployed homepage

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename='/coping-skills-react'>
         <App />
     </Router>
   </React.StrictMode>,
